24285efdf54f20b05892c3ac885edc4cfe9e9ea2
Dev Hyperionixlabs <dev@hyperionixlabs.com>
2026-01-03 10:32:06 -0500

    fix: import AdminModule in FeedbackModule to resolve AdminKeyGuard dependency

--- server/src/feedback/feedback.module.ts
+++ server/src/feedback/feedback.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AdminModule } from '../admin/admin.module';
 import { FeedbackController } from './feedback.controller';
 import { FeedbackAdminController } from './feedback.admin.controller';
 import { FeedbackService } from './feedback.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AdminModule],
   controllers: [FeedbackController, FeedbackAdminController],
   providers: [FeedbackService],
   exports: [FeedbackService],
